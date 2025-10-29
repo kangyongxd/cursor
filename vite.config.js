4e821b304fb0741614eaef1bcc73e47fafbb7b08
kangyong <kangyongxd@163.com>
2025-10-29 16:03:44 +0800

    完善项目

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue2 from '@vitejs/plugin-vue2'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue2()],
+  server: {
+    port: 5173,
+    open: true
+  }
+})
+
+
